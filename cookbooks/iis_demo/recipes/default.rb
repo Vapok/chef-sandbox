12e3240d1264166338310ac49b699a03449d0193
unknown <pnavarra@SEAL-F087NX1-X2.prod.ds.russell.com>
2015-09-29 10:26:44 -0700

    no message

---
+++ cookbooks/iis_demo/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: iis_demo
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+['Web-Server','Web-Common-Http -IncludeAllSubFeature','Web-App-Dev -IncludeAllSubFeature','Web-Mgmt-Tools -IncludeAllSubFeature'].each do |thing|
+	powershell_script "install #{thing}" do
+		code "add-windowsfeature #{thing}"
+	end
+end
+
+service "w3svc" do 
+	action [ :enable, :start ]
+end
+
+#cookbook_file 'c:\inetpub\wwwroot\Default.htm' do
+#	source node["iis_demo"]["indexfile"]
+#	rights :read, "Everyone"
+#end
+
+powershell_script "disable deafult site" do |sitedisable|
+	code 'get-website "Default Web Site*" | where {$_.state -ne "Stopped"} | Stop-Website'
+end
