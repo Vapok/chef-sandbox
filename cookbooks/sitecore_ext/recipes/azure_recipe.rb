e226e183e057c43f04dbdffbf36bcde18725865f
Vapok <VapokRocks@gmail.com>
2015-10-09 17:56:11 -0400

    no message

--- cookbooks/sitecore_ext/recipes/azure_recipe.rb
+++ cookbooks/sitecore_ext/recipes/azure_recipe.rb
@@ -1,5 +1,7 @@
 microsoft_azure = data_bag_item("microsoft_azure", "main")
 
+
+### VM Image Listing
 msazure_expanded_vm_images 'images_listing' do
   management_certificate microsoft_azure['management_certificate'].join("\n")
   subscription_id microsoft_azure['subscription_id']
@@ -19,5 +21,27 @@ ruby_block 'show_images' do
 	    end
   end
   action :run
-  notifies :init, 'msazure_expanded_vm_images[images_listing]', :immediately
+end
+
+
+### VM Disk Listing
+msazure_expanded_vm_disks 'disk_listing' do
+  management_certificate microsoft_azure['management_certificate'].join("\n")
+  subscription_id microsoft_azure['subscription_id']
+  action :list
+end
+
+ruby_block 'show_disks' do
+  block do
+	    r = resources("msazure_expanded_vm_disks[disk_listing]")
+	    disks = r.list_of_disks
+	    if disks.kind_of?(Array) && disks.count > 0
+	    	disks.take(10).each do |disk|
+	    		p disk
+	    	end
+	    else
+	    	puts "Disks not loaded?"
+	    end
+  end
+  action :run
 end
